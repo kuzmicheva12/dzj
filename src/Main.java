13b937e02756c4df199ea54c64db6c991a99ea3b
Kuzmicheva.Evgeniya <you@example.com>
2022-08-03 21:37:45 +0300

    dzj

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2000;
+        int amount = 20;
+        int bonus;
+        bonus = price / amount;
+
+        System.out.println("Бонус: " + bonus );
+
+    }
+}
